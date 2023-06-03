86cfc48eab7a472b84366abc6181f5097482c133
your <jaiswalanshikaajay.7@gmail.com>
2023-06-04 01:20:37 +0530

    UI

---
+++ index.js
@@ -0,0 +1,19 @@
+const inputChange = (e) => { setText(e.target.value) }
+
+
+function handleSubmit() {
+
+}
+
+// UI Controller
+
+const UIctrl = (function () {
+    const UIselectors = {
+        inputLink: '#input_link',
+        shortnbtn: '#submit_btn',
+        errormsg: '#error_msg',
+        copyLink: '#copy_btn',
+        linksContainer: '.links_container'
+    }
+
+});
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      width: {
+        '30': '31%',
+      },
+      colors: {
+        'Dark-Violet': 'hsl(257, 27%, 26%)',
+      },
+      spacing: {
+        '152': '152px',
+        '67': '67px',
+        '66': '66px',
+        '271': '271px',
+        '60': '60px',
+        '82': '82%',
+        '14p': '14%',
+        '106': '106px',
+        '244': '244px',
+        '23': '23%,'
+      },
+    },
+  },
+  plugins: [],
+}
+
